c5fb5a51f455d76b0d0ae64970490232f082f6fb
kevin-wedam <kajongbah1233@gmail.com>
2026-03-03 16:35:29 -0700

    Add set operations methods to SetOperations class
    Implemented set operations using boolean arrays.

--- setOperations/src/setOperations/SetOperations.java
+++ setOperations/src/setOperations/SetOperations.java
@@ -1,25 +1,81 @@
 package setOperations;
 
+/**
+ * Implements ordinary set operations using boolean arrays.
+ * Every position in the array corresponds to an element in the universal set.
+ * A 'true' value means the element is present in the subset.
+ * * Logic based on standard bitwise and boolean array set theory 
+ * Source: concepts from GeeksforGeeks.
+ */
 public class SetOperations {
 
 	/**
-	 * TODO complementNot / NOT(A)
+	 * complement Not / NOT(A) 
+	 * All elements in the universal set that are not in subset A.
+	 * Flips all values in the boolean array.
 	 * @author Kevin
 	 */
+	public static boolean[] complementNot(boolean[] a) {
+		boolean[] result = new boolean[a.length];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = !a[i];
+		}
+		return result;
+	}
+
 	/**
-	 * TODO union / A union B
+	 * union: A union B
+	 * All elements in A and B added together.
+	 * Logical: An element is in the union if it is in A OR B.
 	 * @author Kevin
 	 */
+	public static boolean[] union(boolean[] a, boolean[] b) {
+		boolean[] result = new boolean[a.length];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = a[i] || b[i];
+		}
+		return result;
+	}
+
 	/**
-	 * TODO intersect / A bridge B
+	 * intersect A and B
+	 * All elements that appear in both A and B.
+	 * Logical: An element is in the intersection if it is in A AND B.
 	 * @author Kevin
 	 */
+	public static boolean[] intersect(boolean[] a, boolean[] b) {
+		boolean[] result = new boolean[a.length];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = a[i] && b[i];
+		}
+		return result;
+	}
+
 	/**
-	 * TODO difference / A - B
+	 * difference: A - B
+	 * All elements in A excluding elements that appear in B.
+	 * Logical: True if the element is in A but NOT in B.
 	 * @author Kevin
 	 */
+	public static boolean[] difference(boolean[] a, boolean[] b) {
+		boolean[] result = new boolean[a.length];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = a[i] && !b[i];
+		}
+		return result;
+	}
+
 	/**
-	 * TODO  symmetricDifference / A (+) B
+	 * symmetricDifference: A (+) B
+	 * All elements that appear in A or B but not in both.
+	 * Logical: True if the element is in exactly one of the sets.
 	 * @author Kevin
 	 */
+	public static boolean[] symmetricDifference(boolean[] a, boolean[] b) {
+		boolean[] result = new boolean[a.length];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = a[i] ^ b[i];
+		}
+		return result;
+	}
 }
